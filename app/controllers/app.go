1b43abaa1b2db3bff0f622bf3074f4bc755948fb
Kevin Bowrin <kevin_bowrin@carleton.ca>
2013-07-08 21:19:45 -0400

    GET of public pdf file now working. Messy code, needs cleaning. Too slow.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+    "github.com/robfig/revel"
+	"net/http"
+	"net/url"
+	"io"
+	"io/ioutil"
+	"os/exec"
+	"os"
+	"strings"
+)
 
 type App struct {
 	*revel.Controller
@@ -15,5 +24,85 @@ func (c App) About() revel.Result {
 }
 
 func (c App) Result(pdf string) revel.Result {
-	return c.Render(pdf)
+    
+	c.Validation.Required(pdf).Message("You must provide a url to a pdf.")
+	
+    if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+	
+	url, err := url.Parse(pdf)
+	if err != nil {
+	    c.Validation.Error("Error with PDF URL", err)
+    }
+	
+	//TODO: messy. Maybe defer? Can defer contains returns?
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+	
+	if url.Scheme == ""{
+	    url.Scheme = "http"
+	}
+	
+	resp, err := http.Get(url.String())
+    if err != nil {
+	    c.Validation.Error("HTTP Error with PDF URL", err)
+    }
+	
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}	
+	
+	if resp.StatusCode != http.StatusOK  {
+	    c.Validation.Error("Error loading pdf file.", resp.Status)
+	}
+	
+	if resp.Header["Content-Type"][0] != "application/pdf" {
+	    c.Validation.Error("Error loading pdf file, server not returning pdf content.")
+	}
+	
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+	
+	defer resp.Body.Close()
+	
+	//Create a temporary file, copy the body into it. Then we can run preflight.
+	temp, err := ioutil.TempFile("", "pdfserver")
+	if err != nil{
+	    c.Validation.Error("Error creating temp file.", err)
+	}
+	defer os.Remove(temp.Name())
+	
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+	
+	_, err = io.Copy(temp, resp.Body)	
+	
+	if err != nil{
+	    c.Validation.Error("Error writing to temp file.", err)
+	}
+	
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+	
+	out, _ := exec.Command("java", "-jar", "preflight-app-1.8.2.jar", temp.Name()).Output()
+
+	outSlice := strings.Split(string(out), "\n")
+	return c.Render(pdf, outSlice)
 }
